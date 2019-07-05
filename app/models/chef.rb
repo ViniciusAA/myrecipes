0c98b7866d34b185153f37735875188292ea6470
“Vinícius <vini89go@gmail.com>
2019-07-05 06:00:35 -0300

    Add chef signup

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -4,4 +4,6 @@ class Chef < ApplicationRecord
   validates :chefname, presence: true,length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_many :recipes
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 4 }
 end
