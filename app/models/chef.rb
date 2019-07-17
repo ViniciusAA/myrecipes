6103b917ec2706829a721cc07adb8e85c1ef16d4
“Vinícius <vini89go@gmail.com>
2019-07-17 03:15:48 -0300

    Removed has_secure_password from chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -4,6 +4,5 @@ class Chef < ApplicationRecord
   validates :chefname, presence: true,length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_many :recipes, dependent: :destroy
-  has_secure_password
   validates :password, presence: true, length: { minimum: 4 }, allow_nil: true
 end
